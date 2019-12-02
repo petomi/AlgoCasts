5239449079bcface1ed38a418b9d1a6f832a1efc
Michael Peto <michael_peto@worldvision.ca>
2019-12-02 15:42:05 -0500

    done exercise 1

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// my solution (#1 in answers)
+function reverse(str) {
+    //ezpz if you know Array.reverse() exists
+    return str.split('').reverse().join('');
+}
+
+
+// // OTHER SOLUTIONS:
+// // solution #2
+// function reverse2(str) {
+//     let reversed = '';
+//     // using ES2015 let loop
+//     for (let char of str) {
+//         reversed = char + reversed;
+//     }
+//     return reversed;
+// }
+
+// // solution #3
+// function reverse3(str) {
+//     // ES5.1 reduce method -> reduce array to a single object (reversed), iterating over piece (character), into empty string
+//     return str.split('').reduce((reversed, character) => {
+//         return character + reversed;
+//     }, '');
+// }
 
 module.exports = reverse;
